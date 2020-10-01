ba5b9040c62e52e722dd08387a376051325a9978
Neelakash-gituser <67577710+Neelakash-gituser@users.noreply.github.com>
2020-10-02 01:30:26 +0530

    Update dayoftheprogrammer.py

--- dayoftheprogrammer.py
+++ dayoftheprogrammer.py
@@ -3,6 +3,8 @@ import math
 constant = 243  #sum of days till august 
 n = int(input())
 
+if  n == 1918:
+    constant = 256 - constant + 13
 elif n % 4 == 0 and n < 1918:
     constant += 1
     constant = 256 - constant
